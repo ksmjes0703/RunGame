47e9ac4d779894e3df21184577f271e41446debd
ksmjes0703 <80800105+ksmjes0703@users.noreply.github.com>
2021-04-14 10:48:16 +0900

    4월14일(김승훈)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public bool isGameover = false;
+    public Text scoreText;
+    public GameObject gameoverUI;
+
+    private int score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("씬에 두 개 이상의 게임 매니저가 존재합니다!");
+            Destroy(gameObject);
+        }
+    }
+    public void AddScore(int newScore)
+    {
+        if (!isGameover)
+        {
+            score += newScore;
+            scoreText.text = "Score :" + score;
+        }
+    }
+
+    public void OnplayerDead()
+    {
+
+        isGameover = true;
+        gameoverUI.SetActive(true);
+    }
+}
